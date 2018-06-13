bee32f53b09eb1ce57a9e6c45365d07be4c80514
ChrisZissiadis <chris.zissiadis@gmail.com>
2018-06-13 21:56:32 +1000

    first java code

--- app/src/main/java/com/example/android/dicee/MainActivity.java
+++ app/src/main/java/com/example/android/dicee/MainActivity.java
@@ -1,7 +1,14 @@
 package com.example.android.dicee;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button rollbutton;
+        rollbutton = findViewById(R.id.roll_button);
+        ImageView leftdice = findViewById(R.id.image_leftdice);
+        ImageView rightdice = findViewById(R.id.image_rightdice);
+
+        rollbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Dicee", "button pressed)");
+
+                Random randomNumberGenerator = new Random();
+                int number = randomNumberGenerator.nextInt(6);
+                Log.d("Dicee", "The random number is " +number);
+
+            }
+        });
+
+
     }
 }
